5fc8d417b94dd8eb72bc36fe2365f53f6788da31
Tim Bruijnzeels <tim@nlnetlabs.nl>
2023-09-25 09:29:50 +0200

    Show delete icon for AS0 ROA covering announcement(s).

--- src/components/tables/RoaTableBody.tsx
+++ src/components/tables/RoaTableBody.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Roa, RoaState} from '../../core/types';
+import { Roa, RoaState } from '../../core/types';
 import RoaTableRow from './RoaTableRow';
 
 export interface RoaTableBodyProps {
   tableData: Array<Roa>,
 }
 
-export default function RoaTableBody({tableData}: RoaTableBodyProps) {
+export default function RoaTableBody({ tableData }: RoaTableBodyProps) {
   return (
     <tbody>
       {tableData.map((roa) => (
@@ -14,7 +14,7 @@ export default function RoaTableBody({tableData}: RoaTableBodyProps) {
           key={roa.id}
           roa={roa}
           hasAnnouncements={!!roa.authorizes || !!roa.disallows}
-          allowDelete={roa.state === RoaState.RoaSeen || roa.state === RoaState.RoaDisallowing || roa.state === RoaState.RoaUnseen || roa.state === RoaState.RoaRedundant || roa.state === RoaState.RoaAs0Redundant || roa.state === RoaState.RoaTooPermissive || roa.state === RoaState.RoaNotHeld}
+          allowDelete={roa.state === RoaState.RoaSeen || roa.state == RoaState.RoaAs0 || roa.state === RoaState.RoaDisallowing || roa.state === RoaState.RoaUnseen || roa.state === RoaState.RoaRedundant || roa.state === RoaState.RoaAs0Redundant || roa.state === RoaState.RoaTooPermissive || roa.state === RoaState.RoaNotHeld}
           allowAdd={roa.state === RoaState.AnnouncementNotFound || roa.state === RoaState.AnnouncementInvalidAsn || roa.state === RoaState.AnnouncementInvalidLength}
         />
       ))}
